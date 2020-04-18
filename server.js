c6d332652cb8b1892669f8dc1222d3078cc05db6
Harpreet Singh <singh.harpreet89@hotmail.com>
2020-04-18 01:29:22 -0400

    Updated route file's name

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const dotenv = require('dotenv').config();
 // console.log("ENVIRONMENT VARIABLES: " , dotenv.parsed);
 
 require('./config/database.config')();
-const ShrinkUrlRoutes = require('./app/routes/ShrinkUrl.route');
+const ShrinkUrlRoutes = require('./app/routes/ShrinkUrl.routes');
 const HttpExceptions = require('./app/middlewares/exceptions/HttpExceptions.middleware');
 
 app.use(helmet());
